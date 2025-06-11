b9799dcb2b7644643056381378157ac549aa7c89
cometscome <cometscome@gmail.com>
2025-06-11 09:00:25 +0200

    first commit

--- src/MatrixOnLattice.jl
+++ src/MatrixOnLattice.jl
@@ -1,5 +1,145 @@
 module MatrixOnLattice
+using LinearAlgebra
 
-# Write your package code here.
+abstract type AbstractMatrixOnLattice end
+
+struct MatrixOnLattice4D{NC,T} <: AbstractMatrixOnLattice
+    U::T
+    NC::Int64
+    NX::Int64
+    NY::Int64
+    NZ::Int64
+    NT::Int64
+
+    function MatrixOnLattice4D(NC, NX, NY, NZ, NT)
+        U = zeros(ComplexF64, NC, NC, NX, NY, NZ, NT)
+        T = typeof(U)
+        return new{NC,T}(U, NC, NX, NY, NZ, NT)
+    end
+end
+
+export MatrixOnLattice4D
+
+function Base.zero(M::MatrixOnLattice4D)
+    return MatrixOnLattice4D(M.NC, M.NX, M.NY, M.NZ, M.NT)
+end
+
+function Base.similar(M::MatrixOnLattice4D)
+    return MatrixOnLattice4D(M.NC, M.NX, M.NY, M.NZ, M.NT)
+end
+
+
+
+function applyfunction!(M::MatrixOnLattice4D, f!::Function)
+    for it = 1:M.NT
+        for iz = 1:M.NZ
+            for iy = 1:M.NY
+                for ix = 1:M.NX
+                    f!(M, ix, iy, iz, it)
+                end
+            end
+        end
+    end
+    return
+end
+
+function applyfunction!(M::MatrixOnLattice4D, A::MatrixOnLattice4D, f!::Function)
+    for it = 1:M.NT
+        for iz = 1:M.NZ
+            for iy = 1:M.NY
+                for ix = 1:M.NX
+                    f!(M, A, ix, iy, iz, it)
+                end
+            end
+        end
+    end
+    return
+end
+
+function applyfunction!(M::MatrixOnLattice4D, A::MatrixOnLattice4D, B::MatrixOnLattice4D, f!::Function)
+    for it = 1:M.NT
+        for iz = 1:M.NZ
+            for iy = 1:M.NY
+                for ix = 1:M.NX
+                    f!(M, A, B, ix, iy, iz, it)
+                end
+            end
+        end
+    end
+    return
+end
+
+function applyfunctionsum(M::MatrixOnLattice4D, f::Function)
+    val = 0.0im
+    for it = 1:M.NT
+        for iz = 1:M.NZ
+            for iy = 1:M.NY
+                for ix = 1:M.NX
+                    val += f(M, ix, iy, iz, it)
+                end
+            end
+        end
+    end
+    return val
+end
+
+function randomize!(M::MatrixOnLattice4D{NC,T}, ix, iy, iz, it) where {NC,T}
+    for j in 1:NC
+        for i in 1:NC
+            M.U[i, j, ix, iy, iz, it] = rand(ComplexF64)
+        end
+    end
+end
+
+function Randomfield(NC, NX, NY, NZ, NT)
+    M = MatrixOnLattice4D(NC, NX, NY, NZ, NT)
+    applyfunction!(M, randomize!)
+    return M
+end
+
+function identity!(M::MatrixOnLattice4D{NC,T}, ix, iy, iz, it) where {NC,T}
+    for j in 1:NC
+        for i in 1:NC
+            M.U[i, j, ix, iy, iz, it] = ifelse(i == j, 1.0, 0.0)
+        end
+    end
+end
+
+function multiply!(C::MatrixOnLattice4D{NC,T},
+    A::MatrixOnLattice4D, B::MatrixOnLattice4D, ix, iy, iz, it) where {NC,T}
+    for j in 1:NC
+        for i in 1:NC
+            C.U[i, j, ix, iy, iz, it] = 0.0
+            for k in 1:NC
+                C.U[i, j, ix, iy, iz, it] += A.U[i, k, ix, iy, iz, it] * B.U[k, j, ix, iy, iz, it]
+            end
+        end
+    end
+end
+
+function Identityfield(NC, NX, NY, NZ, NT)
+    M = MatrixOnLattice4D(NC, NX, NY, NZ, NT)
+    applyfunction!(M, identity!)
+    return M
+end
+export Identityfield, Randomfield
+
+function LinearAlgebra.mul!(C::MatrixOnLattice4D, A::MatrixOnLattice4D, B::MatrixOnLattice4D)
+    applyfunction!(C, A, B, multiply!)
+    return C
+end
+
+function traceonlattice(M::MatrixOnLattice4D{NC,T}, ix, iy, iz, it) where {NC,T}
+    val = 0.0im
+    for i in 1:NC
+        val += M.U[i, i, ix, iy, iz, it]
+    end
+    return val
+end
+
+function LinearAlgebra.tr(M::MatrixOnLattice4D)
+    val = applyfunctionsum(M, traceonlattice)
+    return val
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,25 @@
 using MatrixOnLattice
 using Test
+using LinearAlgebra
+
+function test()
+    NX = 16
+    NY = 16
+    NZ = 16
+    NT = 16
+    NC = 3
+
+    U = MatrixOnLattice4D(NC, NX, NY, NZ, NT)
+    U1 = Randomfield(NC, NX, NY, NZ, NT)
+    U2 = Identityfield(NC, NX, NY, NZ, NT)
+    U3 = Identityfield(NC, NX, NY, NZ, NT)
+
+    mul!(U, U2, U3)
+    val = tr(U)
+    println(val)
+end
 
 @testset "MatrixOnLattice.jl" begin
     # Write your tests here.
+    test()
 end
